62427cefeed6dc7e9fd55a4908e6b5afa8247dea
Ian Dugo <6nodder6@gmail.com>
2023-12-24 23:43:40 -0800

    created database of amiibos

---
+++ test.js
@@ -0,0 +1,4 @@
+const fs = require("fs")
+var allAmiibos = require("./data/json-data/amiiboAll.json")
+
+console.log(allAmiibos.series[1])
--- xls2json.js
+++ xls2json.js
@@ -1,17 +1,18 @@
 const fs = require("fs")
 const readline = require("readline")
 
-var jsonOutput = {}
+var jsonOutput = []
 
 if (process.argv.length !== 3) {
     console.error("Expected one command line argument")
     process.exit(1)
 }
 
+var seriesNum = process.argv.at(2)
 
 // for (let series = 1; series < 6; series++) {
 
-const path = process.argv.at(2) == "true" ? `./data/csv-data/AmiiboChecklistSeries${1}.csv` : `./data/csv-data/AmiiboChecklistSeries${process.argv.at(2)}.csv`
+const path = process.argv.at(2) == "true" ? `./data/csv-data/AmiiboChecklistSeries${1}.csv` : `./data/csv-data/AmiiboChecklistSeries${seriesNum}.csv`
 // if (process.argv.at(2) != "true") {
 //     series = 99999;
 // }
@@ -92,8 +93,24 @@ readInterface.on("error", (err) => {
 */
 function toJson(output) {
     for (let i = 1; i < output.length; i++) {
-        for (let j = 0; j < 4; j++) {
-            console.log(output[i][j])
-        }
+        // for (let j = 0; j < 4; j++) {
+        jsonOutput.push({
+            "number": output[i][0],
+            "name": output[i][1],
+            "jp-name": output[i][2],
+            "type": output[i][3],
+            "series": seriesNum,
+            "hasPicture": false,
+            "isFavorite": false,
+            "datePhotoGotten": "01/01/1970"
+        })
+        // }
     }
+    const jsonContent = JSON.stringify(jsonOutput, null, 2);
+    fs.writeFile("./data/json-data/amiiboS" + seriesNum + ".json", jsonContent, "utf8", function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Saved")
+    })
 }
